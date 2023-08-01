32c10fd1fe1e120a0ddcd966868086e22b00d64b
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-01 15:23:34 -0300

    practical finalization of variables

--- js/myJS.js
+++ js/myJS.js
@@ -5,7 +5,7 @@ alert("Un mensaje");
 
 //02-Escribe un programa de una sola línea que escriba en la pantalla un texto que diga "Hello World" (document.write)
 
-document.write("Hola Mundo!");
+document.write("\nHola Mundo!\n");
 
 //03-Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5
 
@@ -14,27 +14,27 @@ document.write(3+5);
 // 04-Escribe un programa de dos lineas que pida el nombre del usuario con un prompt y escriba un texto que diga "Hola nombreUsuario"
 
 let nombreUsuario = prompt("Ingresa tu nombre de usuario");
-document.write("Hola ", nombreUsuario);
+document.write(`\nHola ${nombreUsuario}\n`);
 
 
 // 05-Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 
 let primerN = prompt("Ingresa el primer numero:");
 let segundoN = prompt("Ingresa el segundo numero:");
-document.write("La suma de los numeros es igual a: ",parseInt(primerN)+parseInt(segundoN));
+document.write("\nLa suma de los numeros es igual a: ",parseInt(primerN)+parseInt(segundoN));
 
 // 06-Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 
 // let primerNu = prompt("Ingresa un numero otra vez:");
 // let segundoNu = prompt("Ingresa otro numero otra vez:");
-document.write("El mayor numero ingresado fue:", Math.max (parseInt(primerN), parseInt(segundoN)));
+document.write("\nEl mayor numero ingresado fue:", Math.max (parseInt(primerN), parseInt(segundoN)));
 
 // 07-Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
 
 // let primerNum = prompt("Ingresa un numero otra vez:");
 // let segundoNum = prompt("Ingresa otro numero otra vez:");
 let terserN = prompt("Ingresa el tercer numero:");
-document.write("El mayor numero ingresado fue:", Math.max (parseInt(primerN), parseInt(segundoN), parseInt(terserN)));
+document.write("\nEl mayor numero ingresado fue:", Math.max (parseInt(primerN), parseInt(segundoN), parseInt(terserN)));
 
 // 08-Escribe un programa que pida un número y diga si es divisible por 2
 if (primerN % 2 == 0) {
@@ -45,7 +45,31 @@ document.write("el primer numero es divisible por 2.");
 
 // 09-Escribe un programa que pida una frase y escriba las vocales que aparecen
 
+let frase = prompt('Ingrese una frase');
 
+for (var i=0; i<frase.length; i++) {
+    const caracter=frase.charAt(i,i+1);
+    switch(caracter){
+        case 'a':
+            document.write('a');
+            break;
+        case 'e':
+            document.write('e')
+            break;
+        case 'i':
+            document.write('i')
+            break;
+        case 'o':
+            document.write('o')
+            break;
+        case 'u':
+            document.write('u')
+            break;
+        default:
+            console.log('no tiene vocales');
+            break;
+    }
+}
 
 // 10-Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
 
@@ -62,3 +86,8 @@ if (segundoN % 2 == 0){
 // 11-Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
 
 
+document.write(`El numero ${terserN} es divisible:`);
+for (var i = 1; i < 20; i++) {
+    if(terserN % i == 0)
+    document.write(`por ${i}`);
+}
